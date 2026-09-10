b92aa9ae38d739772687dcce1d3719435535c73e
laptopmannat2346 <tharejamannat1@gmail.com>
2026-09-10 12:23:06 +0530

    changes

--- processors/feed_processor.py
+++ processors/feed_processor.py
@@ -291,7 +291,7 @@ def refresh_articles_from_feeds():
             # and the SQLite writer lock.  The hourly scheduler will collect
             # the remaining entries on subsequent runs.
             batch_size=20,
-            total_batches=1,
+            total_batches=5,
             delay_between_batches=10,
         )
         print_crawl_stats(crawl_stats)
